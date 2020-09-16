dd1636e3fdbe1dac4dffa8cd71eac4e517b2ec1f
cruzaniovillarreal <cruzanio.villarreal@gmail.com>
2020-09-16 12:25:56 -0500

    Reworked

--- js/index.js
+++ js/index.js
@@ -1,26 +1,8 @@
 (function () {
     "use strict";
     $().ready(function () {
-        function fizzbuzz(num) {
-            $('#output').html('')
-            for (var i = 1; i <= num; i++) {
-                if (i % 15 === 0) {
-                    $('#output').append('<li>' + i + ': Fizzbuzz' + '</li>')
-                } else if (i % 3 === 0) {
-                    $('#output').append('<li>' + i + ': Fizz' + '</li>')
-                } else if (i % 5 === 0) {
-                    $('#output').append('<li>' + i + ': Buzz' + '</li>')
-                } else if (isNaN($('#userInput').val())) {
-                    $('#output').html('Please enter a valid number')
-                } else {
-                    $('#output').append('<li>' + i + '</li>')
-                }
-            }
-        }
+        $('.duckT').hide()
+        $('.duckT').delay(1000).slideDown(1000)
 
-        $('#run').click(function () {
-            fizzbuzz($('#userInput').val())
-        })
     });
-
 })();
